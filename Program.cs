fdd9e2cd42b01b6f6abca7b9673f456fd45388d1
Deniz <adenizvayis@gmail.com>
2025-12-23 16:03:58 +0300

    Programlama dersinin not giriş kısmı tamamlandı

--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿Console.Write("Programlama Vize Notunuzu Giriniz : ");
+double vizeNotu = Convert.ToDouble(Console.ReadLine());
+Console.Write("Programlama Final Notunuzu Giriniz : ");
+double finalNotu = Convert.ToDouble(Console.ReadLine());
